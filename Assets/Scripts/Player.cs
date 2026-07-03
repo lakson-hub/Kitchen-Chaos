e41fda82660401afb0950a0cf92947c7cdb126e0
Lakson <lazarmilivojevic0306@gmail.com>
2026-07-03 11:49:01 +0200

    Refactored Player Input code and added separate class for PlayerInput

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,26 +3,13 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     [SerializeField] private float movementSpeed = 7f;
+    [SerializeField] private PlayerInput playerInput;
     
     private bool isWalking;
     
     private void Update() {
-        Vector2 inputVector = new Vector2(0, 0);
+        Vector2 inputVector = playerInput.GetMovementVectorNormalized();
         
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x = +1;
-        }
-        
-        inputVector = inputVector.normalized;
         Vector3 movementDirection = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += movementDirection * (movementSpeed * Time.deltaTime);
         isWalking = movementDirection != Vector3.zero;
