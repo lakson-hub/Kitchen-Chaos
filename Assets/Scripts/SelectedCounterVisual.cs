b2ee4ec0b81f8f445f53ef1f6bd75dd8edcc4961
Lakson <lazarmilivojevic0306@gmail.com>
2026-07-06 13:11:49 +0200

    Refactored SelectedCounterVisual.cs

--- Assets/Scripts/SelectedCounterVisual.cs
+++ Assets/Scripts/SelectedCounterVisual.cs
@@ -1,17 +1,16 @@
-using System;
 using UnityEngine;
 
 public class SelectedCounterVisual : MonoBehaviour {
 
-    [SerializeField] private ClearCounter clearCounter;
-    [SerializeField] private GameObject selectedVisualGameObject;
+    [SerializeField] private BaseCounter baseCounter;
+    [SerializeField] private GameObject[] selectedVisualGameObjectArray;
     
     private void Start() {
         Player.Instance.OnSelectedCounterChanged += Player_OnSelectedCounterChanged;
     }
 
     private void Player_OnSelectedCounterChanged(object sender, Player.OnSelectedCounterChangedEventArgs e) {
-        if (e.selectedCounter == clearCounter) {
+        if (e.selectedCounter == baseCounter) {
             Show();
         } else {
             Hide();
@@ -19,10 +18,14 @@ public class SelectedCounterVisual : MonoBehaviour {
     }
 
     private void Show() {
-        selectedVisualGameObject.SetActive(true);
+        foreach (GameObject selectedVisualGameObject in selectedVisualGameObjectArray) {
+            selectedVisualGameObject.SetActive(true);
+        }
     }
     
     private void Hide() {
-        selectedVisualGameObject.SetActive(false);
+        foreach (GameObject selectedVisualGameObject in selectedVisualGameObjectArray) {
+            selectedVisualGameObject.SetActive(false);
+        }
     }
 }
