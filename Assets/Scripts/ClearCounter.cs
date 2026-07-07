0275a4d0d9f20dd9a826681b4b61f39b09bc1515
Lakson <lazarmilivojevic0306@gmail.com>
2026-07-07 11:50:53 +0200

    Added player putting KitchenObject on ClearCounter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -5,6 +5,14 @@ public class ClearCounter : BaseCounter {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     
     public override void Interact(Player player) {
-        // Pick up and drop items on it, placeholder
+        if (!HasKitchenObject()) {
+            // There is no KitchenObject here!
+            if (player.HasKitchenObject()) {
+                // Player is carrying KitchenObject!
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+        } else {
+            // There is a KitchenObject here!
+        }
     }
 }
