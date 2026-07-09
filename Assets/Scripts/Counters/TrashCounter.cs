e86df3c57cc4c134ea5cfc64a3866f4d7b746a87
Lakson <lazarmilivojevic0306@gmail.com>
2026-07-10 00:19:30 +0200

    Added TrashCounter.cs logic

--- Assets/Scripts/Counters/TrashCounter.cs
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -2,4 +2,9 @@ using UnityEngine;
 
 public class TrashCounter : BaseCounter {
     
+    public override void Interact(Player player) {
+        if (player.HasKitchenObject()) {
+            player.GetKitchenObject().DestroySelf();
+        }
+    }
 }
