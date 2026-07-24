450930c56a6ea8be966109e72a2ce6e93103a696
Lakson <lazarmilivojevic0306@gmail.com>
2026-07-25 01:26:51 +0200

    Added Countdown Timer UI for CountdownToStart state

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,11 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     
     public static GameManager Instance { get; private set; }
+
+    public event EventHandler OnStateChanged; 
     
     private enum State {
         WaitingToStart,
@@ -29,6 +32,7 @@ public class GameManager : MonoBehaviour {
 
                 if (waitingToStartTimer < 0f) {
                     state = State.CountdownToStart;
+                    OnStateChanged?.Invoke(this,EventArgs.Empty);
                 }
 
                 break;
@@ -37,6 +41,7 @@ public class GameManager : MonoBehaviour {
 
                 if (countdownToStartTimer < 0f) {
                     state = State.GamePlaying;
+                    OnStateChanged?.Invoke(this,EventArgs.Empty);
                 }
 
                 break;
@@ -45,6 +50,7 @@ public class GameManager : MonoBehaviour {
 
                 if (gamePlayingTimer < 0f) {
                     state = State.GameOver;
+                    OnStateChanged?.Invoke(this,EventArgs.Empty);
                 }
 
                 break;
@@ -56,5 +62,13 @@ public class GameManager : MonoBehaviour {
 
     public bool IsGamePlaying() {
         return state == State.GamePlaying;
+    }
+
+    public bool IsCountdownToStartActive() {
+        return state == State.CountdownToStart;
+    }
+
+    public float GetCountdownToStartTimer() {
+        return countdownToStartTimer;
     }
 }
