5c5d09d0cab8cf08de0ea5eeda0e9b7edb0eec49
Lakson <lazarmilivojevic0306@gmail.com>
2026-07-28 01:44:35 +0200

    Added LoadingScene with its logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -60,7 +60,6 @@ public class GameManager : MonoBehaviour {
             case State.GameOver:
                 break;
         }
-        Debug.Log(state);
     }
 
     public bool IsGamePlaying() {
--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -10,7 +10,7 @@ public class MainMenuUI : MonoBehaviour {
 
     private void Awake() {
         playButton.onClick.AddListener(() => {
-            SceneManager.LoadScene(1);
+            Loader.Load(Loader.Scene.GameScene);
         });
         
         quitButton.onClick.AddListener(() => {
