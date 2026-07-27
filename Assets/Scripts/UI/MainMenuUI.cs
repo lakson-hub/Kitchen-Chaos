99ae79970cccd815c83e632a8015615030648a43
Lakson <lazarmilivojevic0306@gmail.com>
2026-07-28 01:18:35 +0200

    Added MainMenu scene loading and quitting logic

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,5 +1,20 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour {
-    
+
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+    private void Awake() {
+        playButton.onClick.AddListener(() => {
+            SceneManager.LoadScene(1);
+        });
+        
+        quitButton.onClick.AddListener(() => {
+            Application.Quit();
+        });
+    }
 }
